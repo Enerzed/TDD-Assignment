e4a25ecb7b2ad5d55304eb40ab028c5301d83827
Enerzed <lasker7241@gmail.com>
2025-03-09 23:21:28 +0700

    add TestsRSA - GenerateKeysTest

--- tests.cc
+++ tests.cc
@@ -12,3 +12,13 @@ TEST(TestsRSA, ConstructorTest)
 	RSAEncryption* rsa = new RSAEncryption();
 	EXPECT_NE(rsa, nullptr);
 }
+
+TEST(TestsRSA, GenerateKeysTest)
+{
+	RSAEncryption* rsa = new RSAEncryption();
+	
+	rsa.GenerateKeys();
+
+	EXPECT_NE(rsa.GetPublicKey(), nullptr);
+	EXPECT_NE(rsa.GetPrivateKey(), nullptr);
+}
